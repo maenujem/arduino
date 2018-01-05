dc78397556ffd309c3bda2c5998893182afc666f
maenujem <maenujem@users.noreply.github.com>
2018-01-05 16:08:39 +0100

    Update testNanoPins.ino

--- testNanoPins.ino
+++ testNanoPins.ino
@@ -1,7 +1,13 @@
 /*
-   Test pin soldering (PCB socket pins) for Arduino Nano
-   USB connection only required for sketch upload, but can also be used while running test (14.)
+   Test for pin soldering (PCB socket pins) for Arduino Nano (if you buy it without their "legs" attached and solder these yourself)
+   
    Initial testing using external power source (1. to 13.)
+   The semi-automated test (14.) is based on the built-in pullup-resistors and the built-in LED diode.
+   
+   Required:
+   USB connection: only required for sketch upload, but it can also be used for running the test (14.)
+   Three jumper wires (eg. red 5V, black GND and other color), a breadboard and four 1.2V AA batteries 
+   with connector wire for the test
 */
 
 /*
@@ -35,29 +41,34 @@
         set pin to INPUT
         disconnect wire
 
-   VIN?
-   REF?
+   TODO: testing VIN?
+   TODO: testing REF?
    TODO: D13 not detected correctly (LOW instead HIGH)
    TODO: A6,A7 do not have INPUT_PULLUP (-> floating default values) and are only analogRead()
-
 */
 
 #define LED_PIN 13 // internal LED pin
 #define WAIT_TIME 2000 // in msec
 
-int digitalPins[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-int analogPins[] = {A0, A1, A2, A3, A4, A5, A6, A7}; // A0=14...
+int digitalPins[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}; // no aliases D0.. existing
+int analogPins[] = {A0, A1, A2, A3, A4, A5, A6, A7}; // A0 is alias for pin 14...
 
+/*
+  set all pins in pinarray to INPUT
+*/
 void initPinsAsInput(int pinarr[], size_t arrsize) {
   for (int i = 0; i < arrsize; i++) {
     pinMode(pinarr[i], INPUT);
   }
 }
 
+/*
+  blink n-times per second on specified pin
+*/
 void blink(int blinkPerSecond, int blinkPin) {
   int onTime = 1000 / (blinkPerSecond * 2);
 
-  // init again for sure after digitalWrite on pin 13
+  // init again after possible digitalWrite on pin 13
   pinMode(blinkPin, OUTPUT);
   digitalWrite(blinkPin, LOW);
 
@@ -73,22 +84,27 @@ void blink(int blinkPerSecond, int blinkPin) {
   }
 }
 
+/*
+  check connection of pins in pinarray based on pintype (D/A) and expected state (1/0)
+*/
 void checkPins(int pinarr[], size_t arrsize, char pinType, int checkState) {
   // print what will be tested
   Serial.print(F("Testing "));
-  if (pinType == 'D') {
+  if (pinType == 'D') { // digital pins
     Serial.print(F("digital "));
-  } else if (pinType == 'A') {
+  } else if (pinType == 'A') { // analog pins
     Serial.print(F("analog "));
   }
-  if (checkState == HIGH) {
+  if (checkState == HIGH) { // check for (pullup-resistor) HIGH = no current loss
     Serial.print(F("HIGH: "));
-  } else if (checkState == LOW) {
+  } else if (checkState == LOW) { // check for LOW = pin leads current through wire to GND
     Serial.print(F("LOW: "));
   }
   Serial.println(arrsize);
 
-
+  // print pin type & number, input measured and result to serial
+  // blink three times for each pin when measuring starts
+  // blink once if measurement is ok / twice if measurement is not as expected
   for (int i = 0; i < arrsize; i++) {
     int pinState = 0;
 
@@ -105,7 +121,7 @@ void checkPins(int pinarr[], size_t arrsize, char pinType, int checkState) {
     pinState = (pinType == 'D') ? digitalRead(pinarr[i]) : analogRead(pinarr[i]);
     Serial.print(pinState);
 
-    // 'convert' analog to digital
+    // 'convert' analog measurement (0..1023) to digital (0/1)
     if (pinType == 'A' && pinState > 950) {
       pinState = 1;
     } else if (pinType == 'A' && pinState < 100) {
@@ -126,6 +142,7 @@ void checkPins(int pinarr[], size_t arrsize, char pinType, int checkState) {
 
 void setup() {
   // put your setup code here, to run once:
+   
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
@@ -143,17 +160,17 @@ void setup() {
 void loop() {
   // put your main code here, to run rexpeatedly:
 
-  // checking pin for HIGH (do not connect respective pin to ground with wire)
+  // checking pins 0..21 for HIGH (do not connect respective pin to ground with wire)
   checkPins(digitalPins, sizeof digitalPins / sizeof * digitalPins, 'D', HIGH);
   checkPins(analogPins, sizeof analogPins / sizeof * analogPins, 'A', HIGH);
 
-  blink(4, LED_PIN);
+  blink(4, LED_PIN); // indicate that test for HIGH is done, LOW is next
 
-  // checking pin for LOW (DO connect respective pin to ground with wire)
+  // checking pins 0..21 for LOW (DO connect respective pin to ground with wire)
   checkPins(digitalPins, sizeof digitalPins / sizeof * digitalPins, 'D', LOW);
   checkPins(analogPins, sizeof analogPins / sizeof * analogPins, 'A', LOW);
 
-  blink(4, LED_PIN);
+  blink(4, LED_PIN); // indicate that both tests are done on all pins
   blink(4, LED_PIN);
 }
 
